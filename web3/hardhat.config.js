71a44579074190b19af88266f5efe3c1d927fc77
ikenna <mrteks2022@gmail.com>
2023-07-14 18:19:05 -0700

    first commit

---
+++ web3/hardhat.config.js
@@ -0,0 +1,27 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const PRIVATE_KEY =
+  "a822afef7e6ee700d35c4bf2b6ab278861905cf2af87f0141793ae77a08623de";
+const RPC_URL = "https://rpc.ankr.com/polygon_mumbai";
+module.exports = {
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {
+      chainId: 80001,
+    },
+    polygon_mumbai: {
+      url: "https://rpc.ankr.com/polygon_mumbai",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
+
